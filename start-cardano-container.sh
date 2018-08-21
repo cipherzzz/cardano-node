a97eeacfb45f9918fb8d9a0e54544cf865384874
TheDevKnight <devknight@outlook.com>
2018-08-21 19:50:59 +0200

    disabled nginx

--- start-cardano-container.sh
+++ start-cardano-container.sh
@@ -5,5 +5,6 @@ echo "Docker container ::: Starting node cardano-sl, wallet mode"
 rm -f /home/cardano/cardano-sl/state-wallet-mainnet/wallet-db/open.lock
 
 # restart nginx service in bg, give 10 sec that cardano node create ssl certif.
-sleep 10 && sudo /usr/sbin/service nginx restart && echo "Docker container ::: Nginx service restarted" &
-/home/cardano/cardano-sl/connect-to-mainnet
+#sleep 10 && sudo /usr/sbin/service nginx restart && echo "Docker container ::: Nginx service restarted" &
+
+sudo /home/cardano/cardano-sl/connect-to-mainnet
