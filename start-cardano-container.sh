7787631908ae7e1ac6c15fd512070408566928e2
TheDevKnight <devknight@outlook.com>
2018-08-25 01:44:39 +0200

    remove nginx default config

--- start-cardano-container.sh
+++ start-cardano-container.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "Docker container ::: Starting node cardano-sl, wallet mode"
 
+# remove default nginx config
+sudo rm /etc/nginx/sites-enabled/default
+
 # try to remove lock file from previous execution
-rm -f /home/cardano/cardano-sl/state-wallet-mainnet/wallet-db/open.lock
+sudo rm -f /home/cardano/cardano-sl/state-wallet-mainnet/wallet-db/open.lock
 
 # restart nginx service in bg, give 10 sec that cardano node create ssl certif.
 sleep 10 && sudo /usr/sbin/service nginx restart && echo "Docker container ::: Nginx service restarted" &
 
+# start node
 sudo /home/cardano/cardano-sl/connect-to-mainnet --runtime-args --no-tls
